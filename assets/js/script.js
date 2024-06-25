387617679c712b674f85873d287aa1742ba8ce30
Norbert Tiborcz <norbert.tiborcz@gmail.com>
2024-06-25 19:59:55 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function runGame() {
+
+}
